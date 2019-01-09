d7b344d0dad39b972128532f16c7c2976e7185e8
SahibSinghBanga <sahib_singh_1997@hotmail.com>
2019-01-09 15:13:37 +0530

    Scroll to bottom if user is close t bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');    
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -14,6 +30,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -26,6 +43,7 @@ socket.on('newLocationMessage', function (message) {
     }); 
     
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('disconnect', function () {
@@ -63,3 +81,5 @@ locationButton.on('click', function () {
         alert('Unable to fetch location');
     });
 });
+
+// mustache.js - Logic-less {{mustache}} templates with JavaScript
